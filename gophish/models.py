e15bd3dc99a41218fee6d12b55183d1179802b58
Yegor <h3rmit@protonmail.com>
2021-11-12 11:50:56 +0300

    feat: :sparkles: Add new fields to Campaign model

--- gophish/models.py
+++ gophish/models.py
@@ -70,6 +70,9 @@ class Campaign(Model):
         'redirect_url': None,
         'campaign_template_id': None,
         'email_send_end_date': None,
+        'with_attachment': None,
+        'attachment_type': None,
+        'attachment_name': None,
     }
 
     def __init__(self, **kwargs):
